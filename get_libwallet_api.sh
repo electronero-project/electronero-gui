e1ae3c08005aac86e4e185cf72a96fd047c2994a
Interchained <president@worldvaporexpo.com>
2018-10-03 17:27:09 -0400

    adjustments for release

--- get_libwallet_api.sh
+++ get_libwallet_api.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MONERO_URL=https://github.com/electronero/electronero.git
-MONERO_BRANCH=boost-patch
+MONERO_BRANCH=release
 
 pushd $(pwd)
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -17,7 +17,7 @@ if [ ! -d $MONERO_DIR/src ]; then
 fi
 git submodule update --remote
 git -C $MONERO_DIR fetch
-git -C $MONERO_DIR checkout 12.3.3-omega
+git -C $MONERO_DIR checkout 12.3.5-omega
 
 # get monero core tag
 get_tag
